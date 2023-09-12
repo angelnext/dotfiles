396c7cc55f0543a24f6bffa2d6b9e94fca7d9ae2
Ángel González Vázquez <contact@angelnext.dev>
2023-09-12 17:02:36 +0200

    New computer

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,3 @@
 function ls --wraps='exa --icons -F -H --group-directories-first --git -1' --description 'alias ls exa --icons -F -H --group-directories-first --git -1'
-  exa --icons -F -H --group-directories-first --git -1 $argv
-        
+  eza --icons -F -H --group-directories-first --git -1 $argv     
 end
